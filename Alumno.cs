eb27d6be752106a39411cfffbed05c736e471f01
Armando Rodríguez <jarodriguezvera@gmail.com>
2020-10-23 12:06:23 -0500

    Agregar archivos de proyecto.

---
+++ Alumno.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormularioDatosDeGrupo3
+{
+    class Alumno
+    {
+        private string nombre;
+        private string numeroCuenta;
+        private string materia;
+
+        public string Nombre { get => nombre;
+            set
+            {
+                if( value == ""  )
+                {
+                    nombre = " Juan Carlos Bodoque ";
+                }
+                else
+                {
+                    nombre = value;
+                }
+                
+            }
+        }
+        public string NumeroCuenta {
+            get => numeroCuenta;
+            set
+            {
+                if( value == ""  )
+                {
+
+                }
+                numeroCuenta = value;
+            }
+        }
+        public string Materia { get => materia; set => materia = value; }
+
+
+
+
+        private string GenerarNumeroCuenta()
+        {
+            Random aleatorio = new Random();
+            string numeroCuenta = "";
+            for(int i=0; i < 9; i++ )
+            {                
+                numeroCuenta += aleatorio.Next(9);
+            }
+            return numeroCuenta;
+
+        }
+
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FormularioDatosDeGrupo3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+
+
+}
